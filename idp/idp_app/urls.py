8e45a86052717be5abfb4ba964972b99e46d1b52
waltjos01 <josevinoth.walter@bakerhughes.com>
2025-07-01 12:16:46 +0530

    first_commit

---
+++ idp/idp_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('search_keywords', views.search_keywords, name='search_keywords'),
+    path('manage_pdfs/', views.manage_pdfs, name='manage_pdfs'),
+    path('export_csv/', views.export_csv, name='export_csv'),
+]
